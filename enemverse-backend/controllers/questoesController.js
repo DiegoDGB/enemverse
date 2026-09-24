d33b1d1e75fc2f865b7a48e56640e7e1c338529a
DiegoDGB <bezerradg@gmail.com>
2026-09-24 13:58:37 -0400

    fix: manter respostas sem questão no resumo por área

--- enemverse-backend/controllers/questoesController.js
+++ enemverse-backend/controllers/questoesController.js
@@ -852,7 +852,7 @@ router.get('/historico/resumo', autenticarUsuario, async (req, res) => {
             Resposta.aggregate([
                 { $match: { usuario: new mongoose.Types.ObjectId(usuario) } },
                 { $lookup: { from: Questao.collection.name, localField: 'questao', foreignField: '_id', as: 'dadosQuestao' } },
-                { $unwind: '$dadosQuestao' },
+                { $unwind: { path: '$dadosQuestao', preserveNullAndEmptyArrays: true } },
                 { $group: { _id: { $ifNull: ['$dadosQuestao.area_enem', 'Sem area'] }, respondidas: { $sum: 1 },
                     acertos: { $sum: { $cond: ['$correto', 1, 0] } }, anuladas: { $sum: { $cond: ['$anulada', 1, 0] } } } },
                 { $sort: { _id: 1 } }
@@ -860,7 +860,7 @@ router.get('/historico/resumo', autenticarUsuario, async (req, res) => {
             Resposta.aggregate([
                 { $match: { usuario: new mongoose.Types.ObjectId(usuario) } },
                 { $lookup: { from: Questao.collection.name, localField: 'questao', foreignField: '_id', as: 'dadosQuestao' } },
-                { $unwind: '$dadosQuestao' },
+                { $unwind: { path: '$dadosQuestao', preserveNullAndEmptyArrays: true } },
                 { $group: { _id: { $ifNull: ['$dadosQuestao.materia', 'Sem materia'] }, respondidas: { $sum: 1 },
                     acertos: { $sum: { $cond: ['$correto', 1, 0] } }, anuladas: { $sum: { $cond: ['$anulada', 1, 0] } } } },
                 { $sort: { _id: 1 } }
